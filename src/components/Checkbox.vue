501b78ed07e43537cff0d9db5b2f7c4f14c9d66d
festsnusa <lukatimof@mail.ru>
2023-08-20 18:01:57 +0700

    feat: new component

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,21 @@
+<template>
+  <label class="check-list__label">
+    <input class="check-list__check sr-only" type="checkbox" name="material" value="лен" v-model="checked"
+      @change="checkItem(item.id, checked)">
+    <span class="check-list__desc">
+      {{ item.title }}
+      <span>({{ item.productsCount }})</span>
+    </span>
+  </label>
+</template>
+
+<script>
+export default {
+  props: ["item", "checkItem"],
+  data() {
+    return {
+      checked: false,
+    }
+  },
+}
+</script>
