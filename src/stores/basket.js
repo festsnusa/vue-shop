59979b31762a0dc569156a3883b6ad14d0bb99c8
festsnusa <lukatimof@mail.ru>
2023-08-21 18:34:34 +0700

    feat: addToBasket logic

--- src/stores/basket.js
+++ src/stores/basket.js
@@ -20,6 +20,33 @@ export default defineStore('basket', {
           return;
         });
     },
+    async addToBasket(accessKey, productId, colorId, sizeId, quantity) {
+      const url = `https://vue-moire.skillbox.cc/api/baskets/products?userAccessKey=${accessKey}`;
+      const headers = {
+        accept: 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      };
+      const body = `productId=${productId}&colorId=${colorId}&sizeId=${sizeId}&quantity=${quantity}`;
+
+      try {
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: headers,
+          body: body,
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+          return false;
+        }
+
+        const data = await response.json();
+        return data; // You can do something with the response data here
+      } catch (error) {
+        console.error('Error:', error);
+        return null;
+      }
+    },
   },
   // persist: true,
   persist: {
