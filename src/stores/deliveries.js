ce51314d87cab176ed6c5d88e5cb92756256b72a
festsnusa <lukatimof@mail.ru>
2023-08-22 20:33:12 +0700

    feat: getDeliveries logic

--- src/stores/deliveries.js
+++ src/stores/deliveries.js
@@ -6,6 +6,18 @@ export default defineStore('deliveries', {
       deliveries: [],
     };
   },
+  actions: {
+    async getDeliveries() {
+      await fetch('https://vue-moire.skillbox.cc/api/deliveries')
+        .then((response) => response.json())
+        .then((data) => {
+          this.deliveries = data;
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    },
+  },
   // persist: true,
   persist: {
     storage: sessionStorage,
