e2f104980a33e5e013a0fc37866838a25760e8bc
festsnusa <lukatimof@mail.ru>
2023-08-23 13:36:00 +0700

    feat: new component

---
+++ src/components/DictionaryItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <li class="dictionary__item">
+    <span class="dictionary__key">
+      {{ key }}
+    </span>
+    <span class="dictionary__value">
+      {{ value }}
+    </span>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "DictionaryItem",
+  props: ["key", "value"],
+}
+</script>
