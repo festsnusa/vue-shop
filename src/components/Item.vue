348bc865cc4b329f8cdfd4f1d3fcfe394739f83e
festsnusa <lukatimof@mail.ru>
2023-08-23 15:08:21 +0700

    refactoring: deleted unused images

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -6,19 +6,17 @@
     <section class="item">
       <div class="item__pics pics">
         <div class="pics__wrapper">
-          <img width="570" height="570" :src="currentImage" srcset="img/product-square-1@2x.jpg 2x" alt="Название товара">
+          <img width="570" height="570" :src="currentImage" alt="Название товара">
         </div>
         <ul class="pics__list">
           <li class="pics__item">
             <a href="" class="pics__link pics__link--current">
-              <img width="98" height="98" src="@/assets/img/product-square-2.jpg" srcset="img/product-square-2@2x.jpg 2x"
-                alt="Название товара">
+              <img width="98" height="98" src="@/assets/img/product-square-2.jpg" alt="Название товара">
             </a>
           </li>
           <li class="pics__item">
             <a href="" class="pics__link">
-              <img width="98" height="98" src="@/assets/img/product-square-3.jpg" srcset="img/product-square-3@2x.jpg 2x"
-                alt="Название товара">
+              <img width="98" height="98" src="@/assets/img/product-square-3.jpg" alt="Название товара">
             </a>
           </li>
         </ul>
